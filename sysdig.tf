a752596b40b44b068ff335b9208925c9d52b5656
Tushar Kapadi <tushar.kapadi@gmail.com>
2022-01-28 09:40:05 -0800

    Add files via upload

---
+++ sysdig.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-west-1"
+}
+
+module "secure-for-cloud_example_single-account" {
+  source = "sysdiglabs/secure-for-cloud/aws//examples/single-account"
+
+  sysdig_secure_endpoint = "https://us2.app.sysdig.com"
+  sysdig_secure_api_token = "01d1debd-933a-4a55-9053-86f6225e2e4f"
+}
